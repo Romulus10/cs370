9e075bf8881ff19af3a66e31188df9332b5fad8c
Sean Batzel <stbatzel@gmail.com>
2017-02-21 22:30:47 -0500

    Forgot to update this.

--- project2/lib/matrix.c
+++ project2/lib/matrix.c
@@ -1,3 +1,13 @@
 float *multiply(float *x, float *y) {
+    int i, j;
+}
 
+int *multiply(int *x, int *y) {
+    int i, j;
+    int z[4];
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            x[i][j] = z[j];
+        }
+    }
 }
