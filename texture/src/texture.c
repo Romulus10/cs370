bc743f80312ec72e8c8f180db6af066e6641280a
Sean Batzel <stbatzel@gmail.com>
2017-03-23 14:03:56 -0400

    Another triangle.

--- texture/src/texture.c
+++ texture/src/texture.c
@@ -109,7 +109,7 @@ void draw_triangles() {
     int j = 0;
     //int k = 0;
 
-    for (i = 0; i < 9; i++) {
+    for (i = 0; i < 12; i++) {
         // ...why? Neither j nor k is ever used.
         // just kidding I'm using j now.
         //if (j == 12) {
